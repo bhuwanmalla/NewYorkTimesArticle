77dc9eae3cb7ffb98e519c848436311d74bb4752
bhuwanmalla <bhuwan.malla54@gmail.com>
2024-07-23 16:43:40 -0400

    api key removed

--- app/src/main/java/bhuwan/example/newyorktimes/constants/UrlConst.kt
+++ app/src/main/java/bhuwan/example/newyorktimes/constants/UrlConst.kt
@@ -2,7 +2,7 @@ package bhuwan.example.newyorktimes.constants
 
 object UrlConst {
 
-    private const val API_KEY = "AVKbjzmjdCDN1jxc3FCvZNFebyZWDUgc"
+    private const val API_KEY = "Use your own api key"
     const val BASE_URL = "https://api.nytimes.com/svc/"
     const val ARTICLES_URL = "${BASE_URL}search/v2/articlesearch.json?api-key=${API_KEY}"
     const val TOP_STORIES_URL = "${BASE_URL}topstories/v2/world.json?api-key=${API_KEY}"
